3b108719ce2ce259b836303f472a597a68742983
ted4858 <72432477+ted4858@users.noreply.github.com>
2023-03-15 15:12:29 +0900

    Add database transfer code to FEC_Server.py

--- Python_Server/FEC_Server.py
+++ Python_Server/FEC_Server.py
@@ -2,6 +2,82 @@ import socket
 import threading
 import time
 import random
+import psycopg2
+
+# Connect to the database
+conn = psycopg2.connect(
+    host="localhost",
+    port="3000",
+    database="FEC_DB",
+    user="postgres",
+    password="1234",
+)
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+data = []
+value = []
+data_str = ""
+cycles_per_second = 1
+
+def handle_data():
+    global data
+    global value
+    global data_str
+    global cycles_per_second
+
+    # check database connection
+    while True:
+        try:
+            cur.execute("SELECT 1")
+            conn.commit()
+            print("Database connection is working.")
+            break
+        except psycopg2.OperationalError:
+            print("Database connection error. Retrying...")
+            time.sleep(5)
+
+    # send random values at regular intervals
+    while True:
+        # create 2D array with title, 8 random values, and current time
+        data = [["Channel", "PreBER", "Pre Errors", "Corrected", \
+                 "PostBER", "Margin", "#Bits", "cycles_per_second", "ResultValue", \
+                 "CurrentTime"]]
+        for i in range(4):
+            value = [f"Channel {i+1}"]
+            # 이 반복문에 추후 데이터 계산 후 전송할 수
+            for j in range(8):
+                if (j == 6):
+                    value.append(cycles_per_second)
+                else :
+                    value.append(random.randint(0, 100))
+            value.append(time.strftime("%Y-%m-%d %H:%M:%S"))
+            data.append(value)
+
+            # Insert data into a table
+            cur.execute(f"INSERT INTO Channel{i+1} (preber, \
+                        preerrors, corrected, postber, margin, \
+                        sharpbits, cycles_per_second, result_value, update_time) VALUES \
+                        (%s, %s, %s, %s, %s, %s, %s, %s, %s)", \
+                        (data[i+1][1], int(data[i+1][2]), int(data[i+1][3]), \
+                         data[i+1][4], data[i+1][5], int(data[i+1][6]), \
+                         int(data[i+1][7]), int(data[i+1][8]), data[i+1][9]))
+            # Commit the transaction
+            conn.commit()
+
+        # send value to all clients with message delimiter
+        data_str = "\n".join(["\t".join([str(val) for val in row]) for row in data])
+        
+        print(data)
+        print(data_str)
+
+        # wait for 1 second before sending the next value
+        time.sleep(1)
+
+    # Close the cursor and connection
+    cur.close()
+    conn.close()
 
 def handle_client(client_socket, client_address):
     print('Connection from', client_address)
@@ -14,15 +90,10 @@ def handle_client(client_socket, client_address):
 
     # send random values at regular intervals
     while flag:
-        # generate random value
-        value = random.randint(0, 100)
-        
-        # send value to all clients with message delimiter
-        data = str(value) + '\n'
         with lock:
             for c in clients:
                 try:
-                    c.sendall(data.encode('utf-8'))
+                    c.sendall(data_str.encode('utf-8'))
                 except ConnectionResetError:
                     print(f"Client {client_address} forcibly closed the connection.")
                     clients.remove(c)
@@ -50,6 +121,9 @@ lock = threading.Lock()
 
 # wait for a client to connect
 print('Waiting for a connection...')
+
+data_thread = threading.Thread(target=handle_data)
+data_thread.start()
 
 # loop indefinitely to handle multiple client connections
 while True:
