0f40d94e172a25cbe7d1c7af50ba584dd943b851
ted4858 <72432477+ted4858@users.noreply.github.com>
2023-03-15 15:19:37 +0900

    Add database transfer code to FEC_UI2

--- FEC_UI2/MainWindow.xaml.cs
+++ FEC_UI2/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -28,12 +29,12 @@ namespace FEC_UI2
     /// </summary>
     public partial class MainWindow : Window
     {
-        public ChartValues<float> Values { get; set; }
+        public ChartValues<float>[] Values { get; set; }
 
         private readonly Random _r = new Random();
         private readonly int _delay = 1000;
 
-        Queue DataQ = new Queue();
+        Queue[] DataQ = new Queue[5];
 
         private const string SERVER_ADDRESS = "192.168.1.122";
         private const int SERVER_PORT = 6000;
@@ -41,13 +42,30 @@ namespace FEC_UI2
         private TcpClient client;
         private NetworkStream stream;
 
+        int bytesRead = 0;
+        string data = null;
+
+        // split the data into rows and columns
+        string[] rows = null;
+        string[][] values = null;
+        string[] cols = null;
+
         public MainWindow()
         {
             InitializeComponent();
 
             DataContext = this;
 
-            Values = new ChartValues<float>();
+            Values = new ChartValues<float>[5];
+            for (int i = 0; i < Values.Length; i++)
+            {
+                Values[i] = new ChartValues<float>();
+            }
+
+            for (int i = 0; i < DataQ.Length; i++)
+            {
+                DataQ[i] = new Queue();
+            }
 
             Sync = new object();
 
@@ -70,7 +88,14 @@ namespace FEC_UI2
 
         private async Task InputData()
         {
+            // 지역 변수 초기화
             byte[] buffer = new byte[1024];
+            float[] floatValue = new float[5];
+
+            for (int i = 0; i < floatValue.Length; i++)
+            {
+                floatValue[i] = 0.0f;
+            }
 
             try
             {
@@ -89,22 +114,39 @@ namespace FEC_UI2
                 await Task.Delay(500);
 
                 // receive data from server
-                int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
-                string data = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                data = Encoding.UTF8.GetString(buffer, 0, bytesRead);
 
-                float floatValue;
-
-                lock (Sync)
+                // split the data into rows and columns
+                rows = data.Split('\n');
+                if (rows.Length > 1)
                 {
-                    if (float.TryParse(data, out floatValue))
+                    values = new string[rows.Length][];
+                    for (int i = 1; i < rows.Length; i++)
                     {
-                        Console.WriteLine(floatValue); // Output: 3.14
+                        cols = rows[i].Split('\t');
+                        values[i] = cols;
                     }
-                    else
+
+                    lock (Sync)
                     {
-                        Console.WriteLine("Cannot convert string to float");
+                        for (int i = 1; i < values.Length; i++)
+                        {
+                            if (float.TryParse(values[i][8], out floatValue[i]))
+                            {
+                                Console.WriteLine(floatValue[i]); // Output: 3.14
+                            }
+                            else
+                            {
+                                Console.WriteLine("Cannot convert string to float");
+                            }
+                            DataQ[i].Enqueue((float)floatValue[i]);
+                        }
                     }
-                    DataQ.Enqueue((float)floatValue);
+                }
+                else
+                {
+                    // Handle case where data contains no rows
                 }
             }
         }
@@ -112,38 +154,92 @@ namespace FEC_UI2
         private async Task ReadData()
         {
             await Task.Delay(1000);
- 
-            while (true)
+            try
             {
-                await Task.Delay(_delay);
-                lock (Sync)
+                while (true)
                 {
-                    if (DataQ.Count > 0)
+                    await Task.Delay(_delay);
+
+                    lock (Sync)
                     {
-                        // Dispatcher.Invoke() 메서드를 사용하여 UI 스레드에서 실행
-                        Dispatcher.Invoke(() =>
+                        for (int i = 1; i < values.Length; i++)
                         {
-                            Values.Add((float)DataQ.Dequeue());
-                            if (Values.Count > 120)
+                            if (DataQ[i].Count > 0)
                             {
-                                Values.RemoveAt(0);
+                                // Dispatcher.Invoke() 메서드를 사용하여 UI 스레드에서 실행
+                                Dispatcher.Invoke(() =>
+                                {
+                                    Values[i].Add((float)DataQ[i].Dequeue());
+                                    if (Values[i].Count > 120)
+                                    {
+                                        Values[i].RemoveAt(0);
+                                    }
+
+                                    int index = int.Parse(values[i][0].Replace("Channel ", ""));
+                                    if (index == 1)
+                                    {
+                                        PreBerTextBlock1.Text = values[i][1];
+                                        PreErrorsTextBlock1.Text = values[i][2];
+                                        CorrectedTextBlock1.Text = values[i][3];
+                                        PostBerTextBlock1.Text = values[i][4];
+                                        MarginTextBlock1.Text = values[i][5];
+                                        BitsTextBlock1.Text = values[i][6];
+                                    }
+                                    else if (index == 2)
+                                    {
+                                        PreBerTextBlock2.Text = values[i][1];
+                                        PreErrorsTextBlock2.Text = values[i][2];
+                                        CorrectedTextBlock2.Text = values[i][3];
+                                        PostBerTextBlock2.Text = values[i][4];
+                                        MarginTextBlock2.Text = values[i][5];
+                                        BitsTextBlock2.Text = values[i][6];
+                                    }
+                                    else if (index == 3)
+                                    {
+                                        PreBerTextBlock3.Text = values[i][1];
+                                        PreErrorsTextBlock3.Text = values[i][2];
+                                        CorrectedTextBlock3.Text = values[i][3];
+                                        PostBerTextBlock3.Text = values[i][4];
+                                        MarginTextBlock3.Text = values[i][5];
+                                        BitsTextBlock3.Text = values[i][6];
+                                    }
+                                    else if (index == 4)
+                                    {
+                                        PreBerTextBlock4.Text = values[i][1];
+                                        PreErrorsTextBlock4.Text = values[i][2];
+                                        CorrectedTextBlock4.Text = values[i][3];
+                                        PostBerTextBlock4.Text = values[i][4];
+                                        MarginTextBlock4.Text = values[i][5];
+                                        BitsTextBlock4.Text = values[i][6];
+                                    }
+                                    else
+                                    {
+
+                                    }
+                                });
                             }
-                        });
+                        }
                     }
                 }
             }
-        }
-
-        public string TimeFormatter(TimeSpan time)
-        {
-            return DateTime.Now.ToString("yyyy/MM/dd h:mm:ss tt");
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            // close connection and socket
-            stream?.Close();
-            client?.Close();
+            try
+            {
+                // close connection and socket
+                stream?.Close();
+                client?.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
